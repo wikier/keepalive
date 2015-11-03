8a088d2e56bec1666819be10291e713cd6ebc2e9
Sergio Fernández <sergio@wikier.org>
2015-11-03 14:13:31 +0100

    prepared next dev iteration, if it'd happen at some point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name = 'keepalive',
-      version = 0.1,
+      version = "0.2.dev0",
       description = 'urllib keepalive support',
       long_description = 'An HTTP handler for `urllib2` that supports HTTP 1.1 and keepalive.',
       license = 'GNU GPL',
