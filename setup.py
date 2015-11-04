96d2e816fd56c0948e42adc3dbd08f5037eb48f5
Sergio Fernández <sergio@wikier.org>
2015-11-04 09:08:00 +0100

    fixed setup definition to stress this is a python 3 module (issue #1)

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
       name = 'keepalive',
       version = "0.2.dev0",
-      description = 'urllib keepalive support',
-      long_description = 'An HTTP handler for `urllib2` that supports HTTP 1.1 and keepalive.',
+      description = 'urllib keepalive support for python3',
+      long_description = 'An HTTP handler for `urllib2` that supports HTTP 1.1 and keepalive in Python 3.x.',
       license = 'GNU GPL',
       author = "Benjamin Woodruff",
       author_email = "github@benjam.info",
@@ -17,13 +17,11 @@ setup(
       requires = [],
       install_requires = [],
       classifiers =  [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
       ],
-      keywords = 'python http urllib keepalive',
-      use_2to3 = True,
+      keywords = 'python http urllib keepalive'
 )
